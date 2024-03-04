b0e95d0ef83f00b86f28eee009522a254118d1b8
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-03-04 03:43:13 -0500

    Reformat file

--- Cloak.Core/Cloak.cs
+++ Cloak.Core/Cloak.cs
@@ -10,6 +10,7 @@ public sealed class Cloak(string file)
     internal ModuleDefinition RuntimeModule { get; } =
         ModuleDefinition.FromModule(Assembly.GetCallingAssembly().GetModule("Cloak.Runtime") ??
                                     throw new DllNotFoundException("Runtime module not found"));
+
     public List<Protection> Protections { get; } = [];
 
     public void Protect(string outputDestination)
