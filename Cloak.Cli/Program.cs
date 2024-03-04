d0013ca1ee2faef1f4fd62be921435f161ce46a9
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-03-04 03:14:24 -0500

    Simple cli interface

--- Cloak.Cli/Program.cs
+++ Cloak.Cli/Program.cs
@@ -1,9 +1,30 @@
 ﻿namespace Cloak.Cli;
 
-class Program
+internal static class Program
 {
-    static void Main(string[] args)
+    internal static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Title = "Cloak Obfuscator";
+        Console.Write("Enter Executable Path (.exe/.dll): ");
+        var file = Console.ReadLine()!;
+        var cloak = new Core.Cloak(file);
+        foreach (var protection in cloak.Protections)
+        {
+            Console.Write($"Would you like to enable {protection.Name} (Y/N): ");
+            var answer = Console.ReadLine()!.ToLower().Trim();
+            if (answer != "y" && answer != "n")
+            {
+                Console.WriteLine("Invalid option, press any key to exit");
+                Console.ReadKey();
+                Environment.Exit(-1);
+            }
+
+            protection.Enabled = answer == "y";
+        }
+        Console.Write("Enter target output path: ");
+        var target = Console.ReadLine()!;
+        cloak.Protect(target);
+        Console.WriteLine("Target successfully protected, press any key to exit");
+        Console.ReadKey();
     }
 }
