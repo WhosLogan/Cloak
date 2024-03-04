02384fae71d87280a97ea9b898dadb7503967d25
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-03-04 04:16:52 -0500

    Add RequiredTypes to the Protection base class

--- Cloak.Core/Protections/Protection.cs
+++ Cloak.Core/Protections/Protection.cs
@@ -6,5 +6,7 @@ public abstract class Protection(string name, string description)
     public string Description { get; } = description;
     public bool Enabled { get; set; }
 
+    internal virtual List<string> RequiredTypes { get; } = [];
+
     internal abstract void Execute(Cloak cloak);
 }
