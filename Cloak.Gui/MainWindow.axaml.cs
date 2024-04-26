fe65e47ce3a6e7a9eb34f8865e5043d36035e747
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-04-25 21:04:14 -0400

    Fix protection selection in the ui

--- Cloak.Gui/MainWindow.axaml.cs
+++ Cloak.Gui/MainWindow.axaml.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
+using System.Diagnostics;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Reactive;
+using Cloak.Core.Protections;
 
 namespace Cloak.Gui;
 
@@ -19,15 +21,14 @@ public partial class MainWindow : Window
         Height = 400;
         Width = 350;
         Protections.ItemsSource = _cloak.Protections.ConvertAll(p => p.Name);
-        Protections.GetObservable(ListBox.SelectedItemsProperty)
-            .Subscribe(new AnonymousObserver<IList>(selected => 
-                _cloak.Protections.ForEach(p => p.Enabled = selected.Contains(p.Name)))!);
     }
 
     // ReSharper disable once UnusedParameter.local
     private void ProtectButtonClick(object? sender, RoutedEventArgs e)
     {
         if (string.IsNullOrWhiteSpace(InputBox.Text) || string.IsNullOrWhiteSpace(OutputBox.Text)) return;
+        if (Protections.SelectedItems is not null)
+            _cloak.Protections.ForEach(p => p.Enabled = Protections.SelectedItems.Contains(p.Name));
         _cloak.Protect(InputBox.Text, OutputBox.Text);
     }
 }
